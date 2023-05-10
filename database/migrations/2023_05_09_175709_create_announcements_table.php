22e278505db547e1cfa49844b7cb1e75595e6512
Stefano <stefanociangola@gmail.com>
2023-05-10 20:10:48 +0200

    creata migrazione aggiunta tabella category on announcemets

--- database/migrations/2023_05_09_175709_create_announcements_table.php
+++ database/migrations/2023_05_09_175709_create_announcements_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('body');
+    
             $table->decimal('price', 8, 2 );
             $table->timestamps();
         });
