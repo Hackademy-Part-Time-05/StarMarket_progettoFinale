3fab62d0206654045811e96b2636bfdeca22e6ac
Nicla <niclagallicchio@gmail.com>
2023-05-15 22:00:58 +0200

    aggiunta middleware sul revisore, mancano 8 minuti alla fine della User

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,135 +1,104 @@
 <!-- Remove the container if you want to extend the Footer to full width. -->
 <div class="container-fluid mt-5 bg-dark">
-   <!-- Footer -->
-   <footer
-           class="text-center text-lg-start text-white">
-     <!-- Grid container -->
-     <div class="">
-       <!-- Section: Links -->
-       <section class="">
-         <!--Grid row-->
-         <div class="row">
-           <!-- Grid column -->
-           <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-             <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">
-               Star Market - the shop wars
-             </h6>
-             <p>
-               Trova qui i tuoi oggetti del desiderio, acquista o vendi tutto ciò che è NERD!
-             </p>
-           </div>
-           <!-- Grid column -->
- 
-           <hr class="w-100 clearfix d-md-none" />
- 
-           <!-- Grid column -->
-           <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-             <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">Prodotti</h6>
-             <p>
-               <a class="text-white">MDBootstrap</a>
-             </p>
-             <p>
-               <a class="text-white">MDWordPress</a>
-             </p>
-             <p>
-               <a class="text-white">BrandFlow</a>
-             </p>
-             <p>
-               <a class="text-white">Bootstrap Angular</a>
-             </p>
-           </div>
-           <!-- Grid column -->
- 
-           <hr class="w-100 clearfix d-md-none" />
- 
-           <!-- Grid column -->
-           <hr class="w-100 clearfix d-md-none" />
- 
-           <!-- Grid column -->
-           <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-             <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">Contatti</h6>
-             <p><i class="fas fa-home mr-3"></i> In una galassia lontana lontana</p>
-             <p><i class="fas fa-envelope mr-3"></i> nerd@info.com</p>
-             <p><i class="fas fa-phone mr-3"></i> + 080 234 567 88</p>
-           </div>
-           <!-- Grid column -->
- 
-           <!-- Grid column -->
-           <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-             <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">Seguici</h6>
-            <br>
-             <!-- Facebook -->
-             <a
-                class="btn btn-primary btn-floating m-1"
-                style="background-color: #3b5998"
-                href="#!"
-                role="button"
-                ><i class="fab fa-facebook-f"></i
-               ></a>
- 
-             <!-- Twitter -->
-             <a
-                class="btn btn-primary btn-floating m-1"
-                style="background-color: #55acee"
-                href="#!"
-                role="button"
-                ><i class="fab fa-twitter"></i
-               ></a>
- 
-             <!-- Google -->
-             <a
-                class="btn btn-primary btn-floating m-1"
-                style="background-color: #dd4b39"
-                href="#!"
-                role="button"
-                ><i class="fab fa-google"></i
-               ></a>
- 
-             <!-- Instagram -->
-             <a
-                class="btn btn-primary btn-floating m-1"
-                style="background-color: #ac2bac"
-                href="#!"
-                role="button"
-                ><i class="fab fa-instagram"></i
-               ></a>
- 
-             <!-- Linkedin -->
-             <a
-                class="btn btn-primary btn-floating m-1"
-                style="background-color: #0082ca"
-                href="#!"
-                role="button"
-                ><i class="fab fa-linkedin-in"></i
-               ></a>
-             <!-- Github -->
-             <a
-                class="btn btn-primary btn-floating m-1"
-                style="background-color: #333333"
-                href="#!"
-                role="button"
-                ><i class="fab fa-github"></i
-               ></a>
-           </div>
-         </div>
-         <!--Grid row-->
-       </section>
-       <!-- Section: Links -->
-     </div>
-     <!-- Grid container -->
- 
-     <!-- Copyright -->
-     <div
-          class="text-center"
-          
-          >
-       © 2020 Copyright:
-       <a class="text-white" href="https://mdbootstrap.com/"
-          >MDBootstrap.com</a
-         >
-     </div>
-     <!-- Copyright -->
-   </footer>
-   <!-- Footer -->
- </div>
- <!-- End of .container -->
+    <!-- Footer -->
+    <footer class="text-center text-lg-start text-white">
+        <!-- Grid container -->
+        <div class="">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">
+                            Star Market - the shop wars
+                        </h6>
+                        <p>
+                            Trova qui i tuoi oggetti del desiderio, acquista o vendi tutto ciò che è NERD!
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">Prodotti</h6>
+                        <p>
+                            <a class="text-white">MDBootstrap</a>
+                        </p>
+                        <p>
+                            <a class="text-white">MDWordPress</a>
+                        </p>
+                        <p>
+                            <a class="text-white">BrandFlow</a>
+                        </p>
+                        <p>
+                            <a class="text-white">Bootstrap Angular</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">Contatti</h6>
+                        <p><i class="fas fa-home mr-3"></i> In una galassia lontana lontana</p>
+                        <p><i class="fas fa-envelope mr-3"></i> nerd@info.com</p>
+                        <p><i class="fas fa-phone mr-3"></i> + 080 234 567 88</p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">Passa al lato oscuro: lavora
+                            con noi
+                            {{-- <a href="{{route('become.revisor')}}" class="btn btn-warning text-lighy shadow my-3">Diventa Maestro Jedi</a> --}}
+                        </h6>
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold btn-warning btn">Seguici</h6>
+                            <br>
+                            <!-- Facebook -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!"
+                                role="button"><i class="fab fa-facebook-f"></i></a>
+
+                            <!-- Twitter -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee" href="#!"
+                                role="button"><i class="fab fa-twitter"></i></a>
+
+                            <!-- Google -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!"
+                                role="button"><i class="fab fa-google"></i></a>
+
+                            <!-- Instagram -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!"
+                                role="button"><i class="fab fa-instagram"></i></a>
+
+                            <!-- Linkedin -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca" href="#!"
+                                role="button"><i class="fab fa-linkedin-in"></i></a>
+                            <!-- Github -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #333333" href="#!"
+                                role="button"><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                    <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center">
+            © 2023 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</div>
+<!-- End of .container -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,8 @@
 <x-main>
+    @if(session()->has('access.denied'))
+                <div class="alert alert-danger">{{session('access.denied')}}
+                </div>
+        @endif
     <div class="container">
         <div class="row">
             <div class="col-12">
