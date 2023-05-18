d2b25001cb67c21f3d5462a82289acfda32f3a7d
Stefano <stefanociangola@gmail.com>
2023-05-18 21:24:16 +0200

    correzione navbar e annulamento annunci revisionati spostato

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,5 +1,5 @@
 <x-main>
-    <div class="container-fluid p-5 bg-gradient bg-secondary shadow mb-5">
+    <div class="container-fluid p-5 bg-gradient bg-warning shadow mb-5">
         <div class="row">
            
             <div class="col-12 text-dark m-3">
@@ -11,51 +11,40 @@
                     {{ $announcement_to_check ? 'Ecco l\'annuncio da revisionare:' : 'Non ci sono annunci da revisionare' }}
                 </h1>
             </div>
-            @if ($strazio)
-                
-            <div>{{$strazio->title}}</div>
-            <div class="col-12 col-md-6">
-                <form action="{{route('revisor.cancel_announcement', ['announcement'=>$strazio])}}" method="POST">
-                @csrf
-                @method('PATCH')
-                <button class="btn btn-warning shadow" type="submit">annulla</button>
-                </form>
-            </div>
-            @endif
         </div>
+        
     </div>
-    @if ($announcement_to_check)
-        <div class="container">
-            <div class="row">
-                
-
-
-                <div class="col-md-6 col-12">
-                    <h2 class="neonText2 mb-3">{{$announcement_to_check->title}}</h2>
-                    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                          <div class="carousel-item active">
-                            <img src="https://picsum.photos/1200/700" class="d-block w-100" alt="...">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="https://picsum.photos/1200/701" class="d-block w-100" alt="...">
-                          </div>
-                          <div class="carousel-item">
-                            <img src="https://picsum.photos/1200/699" class="d-block w-100" alt="...">
-                          </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Next</span>
-                        </button>
-                      </div>
-                </div>
     
-                <div class="col-md-6 mt-5 col-12">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-12">
+
+                 @if ($announcement_to_check)
+                    <h2 class="neonText2 mb-3">{{$announcement_to_check->title}}</h2>
+             <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://picsum.photos/1200/700" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/1200/701" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="https://picsum.photos/1200/699" class="d-block w-100" alt="...">
+                </div>
+            </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    
+        <div class="col-md-4 mt-5 ps-5 col-12">
                     <div class="card-body">
                         {{-- <h5 class="card-title mt-3">{{$announcement->title}}</h5> --}}
                         <p class="card-text mt-5">{{$announcement_to_check->body}}</p>
@@ -75,21 +64,26 @@
                                 <button class="btn btn-danger shadow d-inline" type="submit">Rifiuta</button>
                             </form></p>
                       </div>
+                      
                 </div>
-
-
-
-
-
-
-
-
-
-
-
             </div>
-            
+            @if ($strazio)
+           
+            <div class="col-12 col-md-2">
+                 <div>
+                    <h6>
+                        Hai appena revisionato questo annuncio, vuoi annullare?
+                    </h6>{{$strazio->title}}</div>
+                <form action="{{route('revisor.cancel_announcement', ['announcement'=>$strazio])}}" method="POST">
+                @csrf
+                @method('PATCH')
+                <button class="btn btn-warning shadow" type="submit">annulla</button>
+                </form>
+            </div>       
+   
+    @endif
         </div>
+       
         </div>
         @else
         <div class="spazio">
