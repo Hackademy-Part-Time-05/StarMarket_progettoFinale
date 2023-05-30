a437ccab08544596e4bb8cb3a426905a5d685cfe
Francesco-hp <fnardella03@gmail.com>
2023-05-30 20:05:51 +0200

    user story 8 scritta

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use App\Job\RemoveFaces;
 
 class CreateAnnouncement extends Component
 {
@@ -85,9 +86,11 @@ class CreateAnnouncement extends Component
                 $newFileName= "announcements/{$this->announcement->id}";
                 $newImage = $this->announcement->images()->create(['path'=>$image->store($newFileName,'public')]);
 
-                dispatch(new ResizeImage($newImage->path , 400,300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path , 400,300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
